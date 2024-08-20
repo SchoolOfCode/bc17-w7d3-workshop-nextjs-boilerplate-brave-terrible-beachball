6e79a58ec8a8a709517b5fb4daa55f58fa134c77
Jordan <jordanfhoughton@gmail.com>
2024-08-20 12:31:57 +0100

    added events and use state stuff

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,7 +1,37 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import './booking.css'
 
 function Booking() {
+
+    const [ fullName, setFullName ] = useState("")
+    
+    function handleChange (event) {
+        if (event.target.name === "fullName"){
+            setFullName(event.target.value)
+        }
+
+        if (event.target.name === "postcode"){
+            setPostcode(event.target.value)
+        }
+        if (event.target.name === "firstLineAddress"){
+            setFirstLineAddress(event.target.value)
+        }
+        if (event.target.name === "city"){
+            setCity(event.target.value)
+        }
+        if (event.target.name === "phoneNumber"){
+            setPhoneNumber(event.target.value)
+        }
+        if (event.target.name === "email"){
+            setEmail(event.target.value)
+        }
+        
+    }
+
+    console.log(fullName)
+
+
     return (
     <>
     <h1>Design Booking</h1>
@@ -10,26 +40,26 @@ function Booking() {
         <fieldset class="formFieldset">
             
             <label>Full Name
-                <input type="text"></input>
+                <input type="text" name="fullName" onChange={(event) => handleChange(event)}></input>
             </label>
             <label>Postcode
-                <input type="text"></input>
+                <input type="text" name="postcode" onChange={(event) => handleChange(event)}></input>
             </label>
             <label>House/Flat Number and Street Name
-                <input type="text"></input>
+                <input type="text" name="firstLineAddress" onChange={(event) => handleChange(event)}></input>
             </label>
             <label>City
-                <input type="text"></input>
+                <input type="text" name="city" onChange={(event) => handleChange(event)}></input>
             </label>
         </fieldset>
         <h3>Contact Information:</h3>
         <fieldset class="formFieldset">
             
             <label>Phone Number
-                <input type="text"></input>
+                <input type="text" name="phoneNumber" onChange={(event) => handleChange(event)}></input>
             </label>
             <label>Email Address
-                <input type="text"></input>
+                <input type="text" name="email" onChange={(event) => handleChange(event)}></input>
             </label>
         </fieldset>
     </form>
